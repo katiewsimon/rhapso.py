87d83a49832d3be5f963b2a6a56e5f825123bc37
Katie Simon <katiewsimon@gmail.com>
2015-02-28 17:39:02 -0500

    class update

--- get_from_reddit.py
+++ get_from_reddit.py
@@ -1,43 +1,126 @@
 import urllib2
 import json
 
-subreddit = "music"
-sortby = "hot"
-limit = "1"
-
-class rhapsopy(object):
-
-	def get_reddit_data(subreddit,sortby,limit):
-		
-
-def main():
-	target_file = 'reddit_data_for_music'
-	t = open(target_file,'w')
+class Rhapsopy(object):
+	
 	try: 
 		response = urllib2.urlopen("http://reddit.com/r/music/hot.json?limit=3")	
-		content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+		jsonData = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
 	except: 
 		print("Data not found. :(")
-	else:
-		content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
-		children = content['data']['children']
-		heading = 'score,title,title_len,text_len,created_utc'
-		for child in children:
-			child_data = child['data']
-			
-							
 
-			score = child_data['score']
+
+	def __init__(self,jsonData):
+		self.score = jsonData['data']['children']['score']
+#		self.title = jsonData['data']['children']['title']
+		self.title_len = len(jsonData['data']['children']['title']
+		self.text_len = len(jsonData['data']['children']['selftext']
+		self.created_utc = jsonData['data']['children']['created_utc']
+
+		#more
+		# self.subreddit = 'music'
+		# self.sortby = 'hot'
+		# limit = 1
+
+	output_list = []
+
+
+	# methods that convert json input to output usable for music
+	def score_to_music: 
+		return self.score
+
+	def title_len_to_music:
+		return self.title_len
+
+	def text_len_to_music:
+		return self.text_len	
+
+	def time_to_music:
+		return self.created_utc
+
+
+	def to_csv_row:
+		row = ""
+		row += score_to_music, ","
+		row +=
+	
+
+	def get_score(self,subreddit,sortby,limit):
+		
+		else:
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
 			
-			title = child_data['title']
-			title_len = len(child_data['title'])
-			text_len = len(child_data['selftext'])
-			created_utc = child_data['created_utc']
+			children = content['data']['children']
 			
-			new_line = ''
-print "score is: ", score 
-print "title is: ", title
-print " title length is: ", title_len 
-print " text length is: " , text_len
-print " created_utc is: ", created_utc
+			for child in children:
+				child_data = child['data']
+
+		return: child_data['score']
+		
+	def get_title(self,subreddit,sortby,limit):
+		try: 
+			response = urllib2.urlopen("http://reddit.com/r/music/hot.json?limit=3")	
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+		except: 
+			print("Data not found. :(")
+		else:
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+			
+			children = content['data']['children']
+			
+			for child in children:
+				child_data = child['data']
+
+		return: child_data['title']
+	
+	def get_title_len(self,subreddit,sortby,limit):
+		try: 
+			response = urllib2.urlopen("http://reddit.com/r/music/hot.json?limit=3")	
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+		except: 
+			print("Data not found. :(")
+		else:
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+			
+			children = content['data']['children']
+			
+			for child in children:
+				child_data = child['data']
+
+		return: len(child_data['title'])
+
+	def get_text_len(self,subreddit,sortby,limit):
+		try: 
+			response = urllib2.urlopen("http://reddit.com/r/music/hot.json?limit=3")	
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+		except: 
+			print("Data not found. :(")
+		else:
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+			
+			children = content['data']['children']
+			
+			for child in children:
+				child_data = child['data']
+
+		return: len(child_data['selftext']
+
+	def get_created_utc(self,subreddit,sortby,limit):
+		try: 
+			response = urllib2.urlopen("http://reddit.com/r/music/hot.json?limit=3")	
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+		except: 
+			print("Data not found. :(")
+		else:
+			content = json.loads(response.read()).decode(response.info().getparam('charset') or 'utf-8'
+			
+			children = content['data']['children']
+			
+			for child in children:
+				child_data = child['data']
+
+		return: child_data['created_utc']
+
+	
+
 
---
+++ thing.py
@@ -0,0 +1,8 @@
+main:
+	- gget url
+	list = []
+	for childData in children
+		Child child = new Child(childData)
+		list.add(child.to_csv_row)
+
+	print_to_csv(list)
