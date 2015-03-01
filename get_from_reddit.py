7b467251337f367a61f6043b101b6ea74311e7cb
Katie Simon <katiewsimon@gmail.com>
2015-02-28 19:03:26 -0500

    almost done with the Child class

--- get_from_reddit.py
+++ get_from_reddit.py
@@ -1,60 +1,72 @@
 import urllib2
 import json
 
-class Rhapsopy(object):
+class Child(object):
 
 	def __init__(self,jsonData):
 		self.score = jsonData['score']
-#		self.title = jsonData['data']['children']['title']
+		print self.score
 		self.title_len = len(jsonData['title'])
 		self.text_len = len(jsonData['selftext'])
 		self.created_utc = jsonData['created_utc']
 
 	# methods that convert json input to output usable for music
-	def score_to_music(): 
-		return self.score
+	def score_to_instrument(self):
+		self.instrument = int(self.score % 5)
+		return self.instrument
 
-	def title_len_to_pitch():
-		pitch = int(self.title_len % 7)
-		return pitch
+	def title_len_to_pitch(self):
+		self.pitch = int(self.title_len % 7)
+		return self.pitch
 
-	def text_len_to_music():
-		return self.text_len	
+	def text_len_to_duration(self):
+		dur = int(self.text_len % 4)
+		if dur == 0:
+			dur = "whole note"
+		elif dur == 1:
+			dur = "quarter note"
+		elif dur == 2:
+			dur = "half note"
+		elif dur == 3:
+			dur = "dotted half note"
+		return dur	
 
-	def time_to_music():
+	def time_to_music(self):
 		return self.created_utc
 
-
-	def to_csv_row():
+	# adds int input to a row in a csv 
+	def to_csv_row(self):
 		row = ""
-		row += score_to_music, ","
-		row += title_len_to_music, ","
-		row += text_len_to_music, ","
-		row += time_to_music
+		row = row + str(self.score_to_instrument()) + ","
+		row = row + str(self.title_len_to_pitch()) + ","
+		row = row + str(self.text_len_to_duration()) + ","
+		row = row + str(self.time_to_music())
 	
 
 def main():
 
 	target_file = 'reddit_to_music.csv'
 	t = open(target_file,'w')
-	# subreddit = "music"
-	# sortby = "hot"
-	# limit = "1"
 
-	# try: 
+	# possible to customize URL in future:
+	subreddit = "music"
+	sortby = "hot"
+	limit = "1"
+
+	# get json data from reddit
 	response = urllib2.urlopen("http://reddit.com/r/music/hot.json?limit=3")	
 	jsonData = json.loads(response.read())#.decode(response.info().getparam('charset') or 'utf-8')
-	# except: 
-	# 	print("Data not found. :(")
 
+	# navigate to useful data
 	children = jsonData['data']['children']
 
 	output_list = ""
 	row = ""
 
+	# write transformed 
 	for child_data in children:
-		child = Rhapsopy(child_data['data']) 
-		row = str(child.to_csv_row)
+		child = Child(child_data['data']) 
+		row = str(child.to_csv_row())
 		print(row)
 		output_list = output_list + row + '\n'
 
